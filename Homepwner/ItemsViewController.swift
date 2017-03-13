907fe20f739a2a7af94bb7b1bce12cef1b0ec7de
Matthew Olker <molker@spider-man.highpoint.edu>
2017-03-13 00:55:22 -0400

    chapter 10 half way

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     
+    var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Create an instance of UITableViewCell, with default appearance 
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        //Set the text on the cell with the dsecription of the item
+        //that is at the nth index of items, where n = row this cell
+        //will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
 }
